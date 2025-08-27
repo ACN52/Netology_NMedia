2a7961312594f2c56fb34b9e514a0624e1b824b1
ACN <ankrikunov@yandex.ru>
2025-08-27 21:53:00 +0300

    Реализовано ДЗ Buggy Server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,11 @@ android {
         }
     }
 
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
@@ -61,6 +66,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.room)
     implementation(libs.generativeai)
+    implementation(libs.cronet.embedded)
     ksp(libs.androidx.room.compiler)
     implementation(platform(libs.firebase))
     implementation(libs.firebase.messaging)
@@ -68,6 +74,9 @@ dependencies {
     coreLibraryDesugaring(libs.desugaring)
     implementation(libs.okhttp)
     implementation(libs.glide)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.logging.interceptor)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
